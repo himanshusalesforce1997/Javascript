8c58309bad259e4dda0c4baa6af50d8fef2505ae
Vikalp <vikalp1953@gmail.com>
2026-06-17 12:12:04 +0530

    edited project

--- Project/index.js
+++ Project/index.js
@@ -1,1 +1,10 @@
-console.log('Javascript starting')
+console.log([1,-5,3].filter((value, index)=>{
+  if(value>=0){
+    return true;
+  }else{
+    return false;
+  }
+}));
+console.log([1,2,4,-5].map((value, index)=>{
+  return 10;
+}));
